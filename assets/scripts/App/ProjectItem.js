9307abc4133248a1c1033fc53ac77a271c82ab67
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-14 23:58:51 +0300

    imported dynamically Tooltip.js and Component.js by using the import() syntax to download these files only when needed

--- assets/scripts/App/ProjectItem.js
+++ assets/scripts/App/ProjectItem.js
@@ -1,5 +1,5 @@
 import { DOMHelper } from '../Utility/DOMHelper.js';
-import { Tooltip } from './Tooltip.js';
+// import { Tooltip } from './Tooltip.js'; // static import
 
 export class ProjectItem {
   hasActiveTooltip = false;
@@ -18,15 +18,17 @@ export class ProjectItem {
     }
     const projectElement = document.getElementById(this.id);
     const tooltipText = projectElement.dataset.extraInfo;
-    const tooltip = new Tooltip(
-      () => {
-        this.hasActiveTooltip = false;
-      },
-      tooltipText,
-      this.id
-    );
-    tooltip.attach();
-    this.hasActiveTooltip = true;
+    import('./Tooltip.js').then(module => {
+      const tooltip = new module.Tooltip(
+        () => {
+          this.hasActiveTooltip = false;
+        },
+        tooltipText,
+        this.id
+      );
+      tooltip.attach();
+      this.hasActiveTooltip = true;
+    });
   }
 
   connectDrag() {
